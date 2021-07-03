ab71287755e24aa1feca4a90396722932761f4c4
Richie-Yang <richieyang8813@gmail.com>
2021-07-03 18:35:43 +0800

    2021/07/03 0635

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('play/', views.play, name='play'),
     path('result/<int:total>', views.result, name='result'),
-    path('visitorcount', views.visitor_count, name='visitor_count')
+    path('visitorcount/', views.visitor_count, name='visitor_count')
 ]
